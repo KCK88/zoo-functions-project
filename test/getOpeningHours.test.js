1c03127ccc343b53b1f2793c184a76d06a8c871c
Claudio Meira <claudiomeirabs@gmail.com>
2024-02-06 03:29:33 -0300

    Alterando teste da função handlerElephants

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,18 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('Testa se retorna um objeto com todos os horários se a função estiver sem argumentos.', () => {
+    expect(getOpeningHours()).toEqual({
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    });
+  });
+  it('Teste se retorna ', () => {
+    expect(getOpeningHours('Monday', '09:00-AM')).toEqual('The zoo is closed');
+  });
+});
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -2,26 +2,27 @@ const handlerElephants = require('../src/handlerElephants');
 
 describe('Testes da função HandlerElephants', () => {
   it('Verifica se retorna a quantidade de elefantes', () => {
-    expect(handlerElephants('count')).toEqual(4);
+    expect(handlerElephants('count')).toBe(4);
   });
-
   it('Verifica se retorna um array com a relação dos nomes de todos os elefantes', () => {
     expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
   });
-
   it('Verifica se retorna a média de idade dos elefantes', () => {
-    expect(handlerElephants('averageAge')).toEqual(10.5);
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
   });
-
   it('Verifica se retorna a localização dos elefantes dentro do Zoológico', () => {
     expect(handlerElephants('location')).toEqual('NW');
   });
-
   it('Verifica se retorna a popularidade dos elefantes', () => {
-    expect(handlerElephants('popularity')).toEqual(5);
+    expect(handlerElephants('popularity')).toBe(5);
   });
-
   it('Verifica se retorna um array com a relação de dias em que é possível visitar os elefantes', () => {
     expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
+  it('Verifica se retorna undefined ao não passar argumentos', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
+  it('Verifica se retorna undefined ao não passar argumentos', () => {
+    expect(handlerElephants(!String)).toBe('Parâmetro inválido, é necessário uma string');
   });
 });
