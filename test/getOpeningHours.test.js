31eaa94d8968bc50edfeb95bf1a119b0826cf38f
Claudio Meira <claudiomeirabs@gmail.com>
2024-02-06 04:28:54 -0300

    Fazendo testes para a função handlerElephants

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -12,7 +12,25 @@ describe('Testes da função getOpeningHours', () => {
       Monday: { open: 0, close: 0 },
     });
   });
-  it('Teste se retorna ', () => {
-    expect(getOpeningHours('Monday', '09:00-AM')).toEqual('The zoo is closed');
+  it('Testa se retorna "The zoo is closed" "monday" "09:00-AM"', () => {
+    expect(getOpeningHours('monday', '09:00-AM')).toEqual('The zoo is closed');
+  });
+  it('Testa se retorna "The zoo is closed" "tuesday" "09:00-AM"', () => {
+    expect(getOpeningHours('tuesday', '09:00-AM')).toBe('The zoo is open');
+  });
+  it('Testa se retorna "The zoo is closed" "wednesday" "09:00-PM"', () => {
+    expect(getOpeningHours('wednesday', '09:00-PM')).toBe('The zoo is closed');
+  });
+  it('Testa se lança uma exceção "The hour should represent a number"', () => {
+    expect(() => getOpeningHours('saturday', 'HH:MM-AM')).toThrowError('The hour should represent a number');
+  });
+  it('Testa se lança uma exceção "The abbreviation must be AM or PM"', () => {
+    expect(() => getOpeningHours('sunday', '09:00-AA')).toThrowError('The abbreviation must be \'AM\' or \'PM\'');
+  });
+  it('Testa se lança uma exceção "The hour must be between 0 and 12"', () => {
+    expect(() => getOpeningHours('tuesday', '13:00-PM')).toThrowError('The hour must be between 0 and 12');
+  });
+  it('Testa se lança uma exceção "The minutes must be between 0 and 59"', () => {
+    expect(() => getOpeningHours('wednesday', '12:60-PM')).toThrowError('The minutes must be between 0 and 59');
   });
 });
