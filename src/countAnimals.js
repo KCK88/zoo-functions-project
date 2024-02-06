31c880107a461235010d95c471294abd6548cbab
Claudio Meira <claudiomeirabs@gmail.com>
2024-02-06 05:29:23 -0300

    Requisito 5 realizado

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,28 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  const { species, sex } = animal || {};
+
+  if (!species) {
+    const animalCounts = {};
+    data.species.forEach((specie) => {
+      const totalCount = specie.residents.length;
+      animalCounts[specie.name] = totalCount;
+    });
+    return animalCounts;
+  }
+
+  const targetSpecies = data.species.find((specie) => specie.name === species);
+  if (!targetSpecies) {
+    return 'Espécie não encontrada';
+  }
+
+  if (!sex) {
+    return targetSpecies.residents.length;
+  }
+
+  const filteredResidents = targetSpecies.residents.filter((resident) => resident.sex === sex);
+  return filteredResidents.length;
 };
 
 module.exports = countAnimals;
