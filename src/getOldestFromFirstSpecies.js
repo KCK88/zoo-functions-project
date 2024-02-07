23548288bb14cc985a8f0630930d66087da70275
Claudio Meira <claudiomeirabs@gmail.com>
2024-02-06 21:09:47 -0300

    Requisito 10 feiro

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,36 @@
 const data = require('../data/zoo_data');
 
-const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
-};
+function getEmployeeById(id) {
+  return data.employees.find((emp) => emp.id === id);
+}
+
+function getSpeciesById(speciesId) {
+  return data.species.find((species) => species.id === speciesId);
+}
+
+function getOldestAnimal(residents) {
+  return residents.reduce((oldest, current) => {
+    const old = current.age > oldest.age ? current : oldest;
+    return old;
+  });
+}
+
+function getOldestFromFirstSpecies(id) {
+  const employee = getEmployeeById(id);
+
+  if (!employee) {
+    return 'Employee not found';
+  }
+
+  for (const speciesId of employee.responsibleFor) {
+    const species = getSpeciesById(speciesId);
+    if (species) {
+      const oldestAnimal = getOldestAnimal(species.residents);
+      return [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
+    }
+  }
+
+  return 'No species found';
+}
 
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
-const getSchedule = (scheduleTarget) => {
-  // seu código aqui
+const getObjectDays = () => {
+  const arrayDays = Object.keys(data.hours);
+  const objectDays = {};
+  //  const exhibitionArray = data.species.filter((animalDay) =>
+  //    animalDay.availability.includes(day));
+  arrayDays.forEach((weekDay) => {
+    objectDays[weekDay] = {
+      officeHour: `Open from ${data.hours[weekDay].open}am until ${data.hours[weekDay].close}pm`,
+      exhibition: data.species
+        .filter((animalDay) => animalDay.availability.includes(weekDay))
+        .map((animal) => animal.name),
+    };
+  });
+  return objectDays;
 };
 
+const getSchedule = (scheduleTarget) => {
+  if (scheduleTarget === undefined) {
+    return getObjectDays();
+  }
+};
+console.log(getSchedule());
 module.exports = getSchedule;
