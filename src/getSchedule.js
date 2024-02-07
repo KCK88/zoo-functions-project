9fb3a14f23bc0235d2569c65fdacc737e0bf5e83
Claudio Meira <claudiomeirabs@gmail.com>
2024-02-06 23:27:38 -0300

    requisito 9 feito

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,25 +1,38 @@
 const data = require('../data/zoo_data');
 
+const getAnimalDay = (animalName) => {
+  const findAnimal = data.species.find((animal) => animal.name === animalName);
+  return findAnimal ? findAnimal.availability : null;
+};
+
 const getObjectDays = () => {
   const arrayDays = Object.keys(data.hours);
   const objectDays = {};
-  //  const exhibitionArray = data.species.filter((animalDay) =>
-  //    animalDay.availability.includes(day));
   arrayDays.forEach((weekDay) => {
-    objectDays[weekDay] = {
-      officeHour: `Open from ${data.hours[weekDay].open}am until ${data.hours[weekDay].close}pm`,
-      exhibition: data.species
-        .filter((animalDay) => animalDay.availability.includes(weekDay))
-        .map((animal) => animal.name),
-    };
+    if (data.hours[weekDay].open !== 0 && data.hours[weekDay].close !== 0) {
+      objectDays[weekDay] = {
+        officeHour: `Open from ${data.hours[weekDay].open}am until ${data.hours[weekDay].close}pm`,
+        exhibition: data.species
+          .filter((animalDay) => animalDay.availability.includes(weekDay))
+          .map((animal) => animal.name),
+      };
+    } else {
+      objectDays[weekDay] = {
+        exhibition: 'The zoo will be closed!',
+        officeHour: 'CLOSED',
+      };
+    }
   });
   return objectDays;
 };
 
 const getSchedule = (scheduleTarget) => {
-  if (scheduleTarget === undefined) {
-    return getObjectDays();
+  const animalDay = getAnimalDay(scheduleTarget);
+  if (animalDay) {
+    return animalDay;
   }
+  const objectDays = getObjectDays();
+  return objectDays[scheduleTarget] ? { [scheduleTarget]: objectDays[scheduleTarget] } : objectDays;
 };
-console.log(getSchedule());
+console.log(getSchedule(''));
 module.exports = getSchedule;
