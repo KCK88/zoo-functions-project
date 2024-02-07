40e5e1e16fc9805071ccbf70c2b14b50d8230eb9
Claudio Meira <claudiomeirabs@gmail.com>
2024-02-06 23:54:50 -0300

    requisito 11 feito

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,33 @@
 const data = require('../data/zoo_data');
 
-const getEmployeesCoverage = () => {
-  // seu código aqui
-};
+let allEmployee = {};
+function getEmployeesCoverage(ids) {
+  if (ids === undefined) return allEmployee;
+  const testEmployee = allEmployee.find(
+    (enployee) =>
+      enployee.fullName.includes(Object.values(ids))
+      || enployee.id.includes(Object.values(ids)),
+  );
+  if (testEmployee === undefined) {
+    throw new Error('Informações inválidas');
+  }
+  return testEmployee;
+}
+
+allEmployee = data.employees.map((objeto) => {
+  const especiesName = objeto.responsibleFor.map(
+    (id) => data.species.find((specie) => specie.id === id).name,
+  );
+  const especiesLocation = objeto.responsibleFor.map(
+    (id) => data.species.find((specie) => specie.id === id).location,
+  );
+  const employeeArray = {
+    id: objeto.id,
+    fullName: `${objeto.firstName} ${objeto.lastName}`,
+    species: especiesName,
+    locations: especiesLocation,
+  };
+  return employeeArray;
+});
 
 module.exports = getEmployeesCoverage;
